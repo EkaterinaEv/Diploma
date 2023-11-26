672ede01a7079025580935f622b70cc788c68540
Ekaterina Evtyukhova <katyev60@gmail.com>
2023-11-26 20:01:13 +0300

    apihelper commit

---
+++ src/test/java/data/ApiHelper.java
@@ -0,0 +1,27 @@
+package data;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.response.ValidatableResponse;
+import static io.restassured.RestAssured.given;
+public class ApiHelper {
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(8080)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    public static ValidatableResponse sendRequest(String body, int statusCode, String endpoint) {
+        return given()
+                .spec(requestSpec)
+                .body(body)
+                .when()
+                .post(endpoint)
+                .then()
+                .statusCode(statusCode);
+    }
+}
